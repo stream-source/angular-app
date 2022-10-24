59a1656591280baa6daed8df8c996cd9d56a7678
王玄 <wangxuan@xinanli.cn>
2022-10-24 14:11:01 +0800

    feat: three，采用命令生成组件ng g componet 组件名

---
+++ src/app/my-define/three-component/three-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ThreeComponentComponent } from './three-component.component';
+
+describe('ThreeComponentComponent', () => {
+  let component: ThreeComponentComponent;
+  let fixture: ComponentFixture<ThreeComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ThreeComponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ThreeComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/my-define/three-component/three-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-three-component',
+  templateUrl: './three-component.component.html',
+  styleUrls: ['./three-component.component.css']
+})
+export class ThreeComponentComponent implements OnInit {
+
+  constructor() { }
+ 
+  ngOnInit(): void {
+  }
+
+}
