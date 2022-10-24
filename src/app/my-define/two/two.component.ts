ccef982b03ef23b7ebfe8db23fd7ea66b3e7b726
王玄 <wangxuan@xinanli.cn>
2022-10-24 14:09:36 +0800

    feat: two采用分文件方式定义组件：模板+脚本+样式

---
+++ src/app/my-define/two/two.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    templateUrl: './two.component.html',
+    selector: 'app-twoComponet',
+    styleUrls: ['./two.component.css']
+})
+export class TwoComponent {
+
+}
